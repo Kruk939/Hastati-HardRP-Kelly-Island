1ec8bf2c989d13275d03791275d8ab5d9cd69d70
Kruk939 <kajetan@smsnet.pl>
2016-11-15 18:07:01 +0100

    Przekazywanie dowodu

--- @hstlife/addons/hst_life_core/aceConfig.cpp
+++ @hstlife/addons/hst_life_core/aceConfig.cpp
@@ -1,17 +1,20 @@
-/*
+
 class CBA_Extended_EventHandlers;
 class CfgVehicles {
 	class Man;
 	class CAManBase: Man {
-		class ACE_SelfActions {
-			class ACE_Equipment {
-				class LIFE_Phone {
-					displayName = "Otworz telefon";
-					condition = "";
-					statement = QUOTE([] spawn hst_life_phone_functions_fnc_openPhone;);
+		class ACE_Actions {
+			class ACE_MainActions {
+				class LIFE {
+					displayName = "Life";
+					distance = "1";
+					class LIFE_Show_ID {
+						displayName = "Przekaz dowod";
+						statement = QUOTE([1,_target] spawn hst_life_core_fnc_dialogVisaTablet;);
+						priority = 1.0;
+					};
 				};
 			};
 		};
 	};
 };
-*/
